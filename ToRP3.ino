738ce9df299f2b37fae82dec95c90a8408138567
Seon-Je <33336305+Seon-Je@users.noreply.github.com>
2018-01-02 12:41:06 +0900

    Update ToRP3.ino

--- ToRP3.ino
+++ ToRP3.ino
@@ -1,17 +1,46 @@
-int TempPin = A0;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+int Val = 0;  
+char buf[2];  
+int TempPin = A0; 
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
-{
-  Serial.begin(9600);
-}
+{   
+  Serial.begin(9600);    
+  
+  lcd.init();                      // initialize the lcd    
+  lcd.backlight();  // <--> noBacklight(); 
 
-void loop()
-{
-  int Val = analogRead(TempPin);
-  Val = Val * 1000/10 * 5/1024;
+  lcd.setCursor(0,0);    
+  lcd.print("2018-1st");      
+  
+  lcd.setCursor(3,1);   
+  lcd.print("IoT Workshop");    
+  
+  delay(5000); 
+  
+  lcd.clear();    
+  lcd.setCursor(0,0);    
+  lcd.print("*IoT Workshop*");    
+  lcd.setCursor(0,1);    
+  lcd.print("Temp : "); 
+  lcd.setCursor(10,1);    
+  lcd.print("degrees"); 
+} 
 
-  Serial.print("Temp : ");
-  Serial.println(Val);
-  delay(500);
-}
+void loop() 
+{   
+  Val = analogRead(TempPin);   
+  Val = Val * 1000/10 * 5/1024; 
 
+  Serial.print("Temp : ");  
+  Serial.println(Val);  
+
+  lcd.setCursor(7,1);    
+  lcd.print(itoa(Val,buf,10)); 
+
+  delay(500); 
+} 
